11d97448a0611c20b87b6a1d9047f4110e169184
Eshaabhasin <43770452+Eshaabhasin@users.noreply.github.com>
2024-03-15 21:26:33 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int x,y;
+        x=obj.nextInt();
+        y=obj.nextInt();
+        int sum;
+        sum=x+y;
+        System.out.println(sum);
+    }
+}
